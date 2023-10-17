5f1b24f0ef108de9497a69348ff3ed080d35e975
YJ <38809415+0YJ@users.noreply.github.com>
2023-10-17 07:57:31 +0200

    Update hackerLp.py

--- hackerLp.py
+++ hackerLp.py
@@ -16,14 +16,14 @@ logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=loggi
 
 # add argparse to configure YOLOv8 and other parameters if necessary
 parser = argparse.ArgumentParser(description="Smoke detection and fan control")
-parser.add_argument("--weights", type=str, default="yolov8.weights", help="path to the weights file")
-parser.add_argument("--cfg", type=str, default="yolov8.cfg", help="path to the cfg file")
+parser.add_argument("--weights", type=str, default="yolov8v-cls.pt", help="path to the weights file")
+parser.add_argument("--cfg", type=str, default="yolov8.yaml", help="path to the yaml file")
 parser.add_argument("--names", type=str, default="yolov8.names", help="path to the names file")
 args = parser.parse_args()
 
 # load yolo model
 model = YOLO('best.pt')
-#model.train(data='xxxx', epoh=100)
+#model.train(data='...', epoh=100)
 
 # validation 
 metrics = model.val()
@@ -34,7 +34,7 @@ results = model.predict("xx")
 probs = result.probs
 print(probs.data)
 
-tensor([xxx])
+tensor([...])
 # control the fan based on smoke density
 def control_fan(smoke_density):
     if smoke_density == 0:
@@ -42,21 +42,19 @@ def control_fan(smoke_density):
         logging.info("No smoke detected, fan stopped.")
     elif smoke_density == 1:
         # middle smoke detected, fan running at low speed
-        logging.info("Light smoke detected, fan running at low speed.")
+        logging.info("Mid smoke detected, fan running at low speed.")
     elif smoke_density == 2:
         # full smoke detected, fan running at full speed
-        logging.info("Heavy smoke detected, fan running at full speed")
+        logging.info("Full smoke detected, fan running at full speed")
 
 try:
     while True:
-        # receive the Base64 encoded frame from the Raspberry Pi
+        # receive base64 encoded frame from the Raspberry Pi
         frame_as_base64 = socket.recv()
         
         # decode the frame from Base64
         buffer = base64.b64decode(frame_as_base64)
         frame = cv2.imdecode(np.frombuffer(buffer, np.uint8), -1)
-
-        # continue with smoke density calculation as in your original code
 
         # detect objects, specifically "smoke," in the frame using YOLOv8
         results = yolo.detect(frame)
